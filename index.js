4d654fae71100328f6b4a66ad32a714e4f7e315c
jacquelineturof <jacquelineturof@gmail.com>
2021-02-03 16:01:35 -0500

    adding web sockets

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ dotenv.config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
-
+const cors = require('cors')
+const MessagingResponse = require('twilio').twiml.MessagingResponse
+const wsServer = require('./socket')
+console.log(wsServer.connections)
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
@@ -20,21 +23,38 @@ const client = require('twilio')(
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
+app.use(cors())
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json')
     next()
 })
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+app.post('/api/sms', async (req, res) => {
+    const twiml = new MessagingResponse()
+    const content = req.body.Body
+    console.log('content: ', content)
+
+    try {
+        wsServer.connections[0].sendUTF(content)
+        if (req.body.Body == 'test') {
+            twiml.message('Hi!')
+        } else {
+            twiml.message('Bye!')
+        }
+        
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        res.end(twiml.toString());
+    } catch (e) {
+        console.log(e)
+        res.status(500).send('fail!')
+    }
+})
 
 app.post('/api/messages', async (req, res) => {
     try {
         await client.messages.create({
             from: process.env.TWILIO_PHONE_NUMBER,
-            to: req.body.to,
+            to: process.env.MY_NUMBER,
             body: req.body.body
         })
 
